cdeee80f7fec31f9e301526a3553f54ebbe74f15
bhanu <udaibhanu2602@gmail.com>
2022-09-23 02:39:19 +0530

    Setting CSS class Dynamically Task-1(2)

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,8 +3,10 @@ import React from 'react';
 import './Button.css';
 
 const Button = props => {
+  let buttonClass='button '+props.className
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    
+    <button type={props.type} className={buttonClass} onClick={props.onClick}>
       {props.children}
     </button>
   );
